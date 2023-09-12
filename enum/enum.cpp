90883650477c500647b2e55e8f7399e72c759199
Loic Tlpk <tulpinckloic@gmail.com>
2023-09-12 11:13:48 +0200

    structTest

---
+++ enum/enum.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+struct points{
+    int loic = 0 ; 
+    int eric = 0 ;
+    int hasan = 0 ;
+} pt;
+
+int display(){
+    
+    for (int* ptr = &pt.loic; ptr <= &pt.hasan; ptr++) {
+        std::cout << "Valeur : " << *ptr << std::endl;
+    }
+    return 0;
+}
+
+int changeValue(){
+    pt.loic = 0;
+    pt.eric = 0;
+    pt.hasan = 0;
+    return 0;
+};
+
+int changeValue(int a){
+    pt.loic = a;
+    return 0;
+};
+
+int changeValue(int a, int b){
+    pt.loic = a;
+    pt.eric = b;
+    return 0;
+};
+
+int changeValue(int a, int b, int c){
+    pt.loic = a;
+    pt.eric = b;
+    pt.hasan = c;
+    return 0;
+};
+
+int main(){
+    display();
+    changeValue(1, 4, 6);
+    display();
+    return 0;
+}
